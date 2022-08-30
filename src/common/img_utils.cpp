f0cff097b90c423968ed1fb092aba48cca43eea7
csb1024 <csb1024@kaist.ac.kr>
2022-08-30 20:16:56 +0900

    feat: added resizing code

--- src/common/img_utils.cpp
+++ src/common/img_utils.cpp
@@ -5,12 +5,30 @@
 
 // Resize an image to a given size to
 cv::Mat __resize_to_a_size(cv::Mat &image, int new_height, int new_width){
+  // get original image size
+  int org_image_height = image.rows;
+  int org_image_width = image.cols;
+  // get image area and resized image area
+  float img_area = float(org_image_height * org_image_width);
+  float new_area = float(new_height * new_width);
+  // resize
+  cv::Mat image_scaled;
+  cv::Size scale(new_width, new_height);
+  if (new_area >= img_area) {
+    cv::resize(image, image_scaled, scale, 0, 0, cv::INTER_LANCZOS4);
+  } else {
+    cv::resize(image, image_scaled, scale, 0, 0, cv::INTER_AREA);
+  }
+  return image_scaled;
+
 }
 
+// Normalize an image by subtracting mean and dividing by standard deviation
 cv::Mat __normalize_mean_std(cv::Mat &image, std::vector<double> mean, std::vector<double> std){
+  
 }
 
-// 1. Preprocess
+// Preprocess
 cv::Mat preprocess(cv::Mat *image, int new_height, int new_width) 
 {
   std::vector<double> mean = {0.485, 0.456, 0.406};
