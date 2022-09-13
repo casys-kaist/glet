a16791a29ddfb5d672f5138d527e18f30a46ccfd
csb1024 <csb1024@kaist.ac.kr>
2022-09-13 16:57:45 +0900

    initial commit: skeleton code for implementing interference model

---
+++ src/scheduler/interference_model.cpp
@@ -0,0 +1,25 @@
+#include "interference_model.h"
+#include <assert.h>
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace InterferenceModeling;
+
+
+void InterferenceModeling::InterferenceModel::setup(std::string model_const_file, std::string util_file)
+{
+
+}
+
+void InterferenceModeling::InterferenceModel::_setup(std::string input_file1, std::string input_file2)
+{
+		
+
+}
+
+double InterferenceModeling::InterferenceModel::getInterference(std::string my_model, int my_batch, int my_partition, std::string your_model, int your_batch, int your_partition)
+{
+ 
+
+}
