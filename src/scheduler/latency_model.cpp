ea38b50b16fc2643fce34ebeefe16a2ea4fac9e5
csb1024 <csb1024@kaist.ac.kr>
2022-09-13 17:18:01 +0900

    feat: code for setting up table

--- src/scheduler/latency_model.cpp
+++ src/scheduler/latency_model.cpp
@@ -11,6 +11,34 @@
 
 
 void LatencyModel::setupTable(std::string TableFile){
+    std::string str_buf;
+    std::ifstream file(TableFile);
+    std::string line;
+    #ifdef DEBUG
+    std::cout << __func__ << " called for " << TableFile << std::endl;
+    #endif
+    while(std::getline(file, line)){
+        std::istringstream iss(line);
+        std::string field;
+        Entry new_entry;
+		std::unordered_map<int,float> per_entry_latency;
+        std::getline(iss, field,',');
+        std::string model=field;
+		std::map<std::string,std::unordered_map<int,float>*>::iterator it = _perModelLatnecyTable.find(model);
+		if(it == _perModelLatnecyTable.end()){
+			_perModelLatnecyTable[model]=new std::unordered_map<int,float>();
+            _perModelGPURatioTable[model]=new std::unordered_map<int,float>();
+		}
+	    std::getline(iss, field,',');
+        new_entry.part=stoi(field);
+        std::getline(iss, field,',');
+        new_entry.batch=stoi(field);
+        std::getline(iss, field,',');
+        new_entry.latency=stof(field);
+	    int key=makeKey(new_entry.batch, new_entry.part);
+        _perModelLatnecyTable[model]->operator[](key)=new_entry.latency;
+        _perModelBatchVec[model][new_entry.part].push_back(new_entry.batch);      
+    } 
 }
 // acts like an hash key
 int LatencyModel::makeKey(int batch, int part){ 
