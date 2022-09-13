0a28fa7efdc2aa86f5de4661585bc84f2bb2a2f8
csb1024 <csb1024@kaist.ac.kr>
2022-09-13 18:54:13 +0900

    initial commit: skeleton code for implementing load balancer

---
+++ src/service/load_balancer.cpp
@@ -0,0 +1,65 @@
+#include "load_balancer.h"
+#include "common_utils.h"
+#include <iostream>
+#include <random>
+
+#define EPOCH_IN_MILLIS 500
+#define MIN_EPOCH 100
+#define MAX_EPOCH 1000
+
+// constants used for shifting when making keys
+#define MODEL_ID_SHIFT 10000000
+#define PART_SHIFT 1000
+#define GPU_ID_SHIFT 10
+
+LoadBalancer::LoadBalancer(){
+}
+
+LoadBalancer::~LoadBalancer(){}
+void LoadBalancer::clearTables(){
+}
+
+void LoadBalancer::setType(std::string balancer_type){
+}
+
+// wrapper function: updates internal tables, returns whether successful or not 
+int LoadBalancer::updateTable(SimState &sched_results){
+}
+
+
+int LoadBalancer::updateTable(std::map<proxy_info *, std::vector<std::pair<int, double>>> &trps){
+}
+
+int LoadBalancer::renewTable(int model_id, int key, double trpt){
+
+}
+
+int LoadBalancer::updateWRR(SimState &sched_results){
+}
+
+proxy_info* LoadBalancer::choseProxy(int model_id){
+}
+
+
+// wrapper function: returns whether the proxy is supposed to take the task(model_name)
+bool LoadBalancer::checkLoad(int model_id, proxy_info* pPInfo){
+}
+
+bool LoadBalancer::checkWRR(int model_id, proxy_info* pPInfo){
+}
+
+
+int LoadBalancer::getKey(int model_id, proxy_info* pPInfo){
+}
+
+// returns hash key
+int LoadBalancer::getKey(int model_id, int gpu_id, int part, int dedup_num){
+}
+
+bool LoadBalancer::containModelID(int key, int model_id){
+}
+int LoadBalancer::checknCreateMtx(int model_id){
+}
+// returns whether the credit hold by 'key' for model_id is smaller than min
+bool LoadBalancer::checkMinCredit(int model_id, int key){
+}
