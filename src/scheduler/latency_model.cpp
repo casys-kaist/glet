592e71a738d11e95b3551d96a2055ecb9e1daf8e
csb1024 <csb1024@kaist.ac.kr>
2022-09-13 17:23:00 +0900

    feat: add linear searching/interpolation code for estimating latency

--- src/scheduler/latency_model.cpp
+++ src/scheduler/latency_model.cpp
@@ -82,9 +82,50 @@ std::pair<int,int> findBatchpair(std::vector<int> &list, int batch, int part)
 
 
  float LatencyModel::getBatchPartInterpolatedLatency(std::string model, int batch, int part){
+     std::vector<int> keys_vec;
+    for(std::unordered_map<int,float>::iterator it = _perModelLatnecyTable[model]->begin();
+        it != _perModelLatnecyTable[model]->end();it++ )
+    {
+        keys_vec.push_back(it->first);
+    }
+    assert(keys_vec.size() >= 2);
+    sort(keys_vec.begin(), keys_vec.end());
+    Entry* temp = parseKey(keys_vec.front());
+    int min_part = temp->part;
+    delete temp;
+    temp = parseKey(keys_vec.back());
+    int max_part = temp->part;
+    delete temp;
+    // assume that every part has max batch size profiled
+    int temp_key1 = makeKey(MAX_BATCH,min_part);
+    int temp_key2 = makeKey(MAX_BATCH,part);
+    int temp_key3 = makeKey(MAX_BATCH,max_part);
+    float y1 = getBatchInterpolatedLatency(model,MAX_BATCH,min_part);
+    float y =  getBatchInterpolatedLatency(model,MAX_BATCH,part);
+    float y2 = getBatchInterpolatedLatency(model,MAX_BATCH,max_part);
+    float b=(y-y2)/(y1-y2);
+    y1=getBatchInterpolatedLatency(model,batch,min_part);
+    y2=getBatchInterpolatedLatency(model,batch,max_part);
+    float diff=y1-y2;
+    return diff*b + y2;
  }
 
  float LatencyModel::getBatchInterpolatedLatency(std::string model, int batch, int part){
+    uint64_t p1,p2,p3,p4;
+    // if batch is in the table, lookup and return
+    if(batch == MIN_BATCH || batch == MAX_BATCH){
+        return _perModelLatnecyTable[model]->operator[](makeKey(batch,part));
+    } 
+    // if not, do interpolation
+    std::pair<int,int> two_batch = findBatchpair(_perModelBatchVec[model][part], batch, part);
+
+    int b1 = two_batch.first;
+    int b2 = two_batch.second;
+    float l1=_perModelLatnecyTable[model]->operator[](makeKey(b1,part));
+    float l2=_perModelLatnecyTable[model]->operator[](makeKey(b2,part));
+    assert(l1 != 0.0 && l2 != 0.0);
+    float ret_latency = (l2-l1)/float(b2-b1) * (batch-b1) + l1;
+    return ret_latency;
  }
 
 float LatencyModel::getLatency(std::string model, int batch, int part){
