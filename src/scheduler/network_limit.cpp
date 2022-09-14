569e8a1265487915fb8258c9b302cb68079be0a3
csb1024 <csb1024@kaist.ac.kr>
2022-09-14 16:29:09 +0900

    inital commit: skeleton code for checking network limit

---
+++ src/scheduler/network_limit.cpp
@@ -0,0 +1,14 @@
+#include "network_limit.h"
+#include <iostream>
+#include "json/json.h"
+#include <assert.h>
+
+NetworkLimitChecker::NetworkLimitChecker(){}
+NetworkLimitChecker::~NetworkLimitChecker(){}
+int NetworkLimitChecker::setupPerTaskInputDimension(std::string json_file){
+  
+}
+
+int NetworkLimitChecker::adjustBatchSizetoNetBW(TaskPtr &task_ptr, GPUPtr &to_be_inserted_gpu){
+ 
+}
