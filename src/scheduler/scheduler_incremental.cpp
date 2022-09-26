8133b5f02167bfa13352de39c798214fb6246645
csb1024 <csb1024@kaist.ac.kr>
2022-09-26 19:01:14 +0900

    feat: main logic of elastic partitioning

--- src/scheduler/scheduler_incremental.cpp
+++ src/scheduler/scheduler_incremental.cpp
@@ -180,9 +180,164 @@ namespace Scheduling{
 			temp_node_ptr->occupancy= latency / temp_node_ptr->duty_cycle;
 			temp_node_ptr->vTaskList.push_back(temp_task_ptr);
 			output_vec.push_back(temp_node_ptr);
+	}
+}
+
+bool cmp_nodeptr_dsc(const NodePtr &a, const NodePtr &b){
+	return a->resource_pntg > b->resource_pntg;
+}
+
+
+bool cmp_nodeptr_asc(const NodePtr &a, const NodePtr &b){
+	return a->resource_pntg < b->resource_pntg;
+}
+
+bool isModelScheduled(Task &task, SimState &decision){
+	bool ret_val = false;
+	for(auto gpu_ptr : decision.vGPUList){
+		for(auto node_ptr : gpu_ptr->vNodeList){
+			for(auto task_ptr: node_ptr->vTaskList){
+				if(task.id == task_ptr->id) ret_val=true;
+			}
 		}
 	}
+	return ret_val;
+}
 
+bool IncrementalScheduler::addModeltoSchedule(Task &task, SimState &decision){
+			bool split=false;
+			bool good_to_go=false;
+			bool failed=false;
+			task.additional_rate=0;
+			// allocate min part, until this task does not require more gpu-lets
+			// allocate saturate
+			int prev_rate=task.request_rate;
+			std::vector<NodePtr> estimate_result;
+			bool scheduled=isModelScheduled(task,decision);
+#ifdef SCHED_DEBUG
+			std::cout << __func__ << "model: " << task.id << " scheduled?: " << scheduled
+				<<std::endl;
+#endif
+			if(!scheduled){
+				bool goto_adjust=false;
+				getEstimate(task, estimate_result, getMaxPartSize(decision));
+				//sort(estimate_result.begin(), estimate_result.end(),cmp_nodeptr_asc);
+				sort(estimate_result.begin(), estimate_result.end(),cmp_nodeptr_dsc);
+				if(!checkFit(estimate_result,decision)){
+					if(allocateFit(estimate_result,task,decision)){
+						goto_adjust=true;
+					}
+				}
+				else goto_adjust=true;
+#ifdef SCHED_DEBUG
+				std::cout << "AFTER checkFit: " << std::endl;
+				printResults(decision);
+#endif
+
+				if(goto_adjust){ // if greedy fit failed
+					// get empty partitions
+					std::vector<NodePtr> empty_nodes;
+					for(auto gpu_ptr : decision.vGPUList){
+						for(auto node_ptr: gpu_ptr->vNodeList){
+							if(node_ptr->vTaskList.empty()){
+								empty_nodes.push_back(node_ptr);
+							}
+						}
+					}
+					if( !empty_nodes.empty()){
+						// sort empty_nodes in descending order with respect to pntg
+						sort(empty_nodes.begin(), empty_nodes.end(), cmp_nodeptr_dsc);
+					}
+					readjust(task,empty_nodes, decision);
+#ifdef SCHED_DEBUG
+					std::cout << "AFTER readjust: " << std::endl;
+					printResults(decision);
+#endif 
+
+					if (!mergeResidue(task,decision)) return EXIT_SUCCESS;
+#ifdef SCHED_DEBUG
+					std::cout << "AFTER mergeResidue: " << std::endl;
+					printResults(decision);
+#endif
+
+					failed=true;
+					if(!_useSelfTuning){
+						std::vector<NodePtr> empty_vec;
+						empty_vec.clear(); // just to make sure
+						if(!allocateTimeShare(task,decision, empty_vec)){
+							failed=false;
+						}
+					}
+					if(failed) return EXIT_FAILURE;
+				}
+			} // scheduled
+			else{ // if this task was previously scheduled
+				bool failed = false;
+				std::vector<NodePtr> target_nodes;
+				// nodes used when time sharing
+				std::vector<NodePtr> ts_target_nodes;
+				// Retrieve and clear nodesa
+				// when clearing nodes that time share-task, be sure to just erase task only and not add to target_nodes
+				std::cout << "AFTER SCHEDULED START: " << std::endl;
+				printResults(decision);
+				for(auto gpu_ptr : decision.vGPUList){
+					for(auto node_ptr: gpu_ptr->vNodeList){
+						printNodeInfo(node_ptr);
+						if(node_ptr->vTaskList.empty()) target_nodes.push_back(node_ptr);
+						else{
+							bool found=false;
+							for(int i =0; i < node_ptr->vTaskList.size(); i++){
+								if(node_ptr->vTaskList[i]->id == task.id){
+									node_ptr->vTaskList.erase(node_ptr->vTaskList.begin()+i);
+									ts_target_nodes.push_back(node_ptr);
+									found=true;
+									break;
+								}
+							}
+							// update occupancy
+							float latency_sum=0;
+							for(auto task_ptr: node_ptr->vTaskList){
+								latency_sum += getLatency(node_ptr->type,task_ptr->id,task_ptr->batch_size,node_ptr,decision);
+							}
+							node_ptr->occupancy = (latency_sum / node_ptr->duty_cycle);
+							// if "task" was the only task scheduled then add, if not then just continue(and leave it to allocateTimeShare if neccessasry)
+							if(node_ptr->vTaskList.empty()) target_nodes.push_back(node_ptr); 
+						}
+					}
+				}
+				if(!target_nodes.empty()) sort(target_nodes.begin(), target_nodes.end(), cmp_nodeptr_dsc);
+
+#ifdef SCHED_DEBUG
+				std::cout <<"task: "<< task.id << " Len of target nodes: " << target_nodes.size() <<std::endl;
+				std::cout <<"task: "<< task.id << " Len of ts target nodes: " << ts_target_nodes.size() <<std::endl;
+				for(auto n_ptr : target_nodes){
+					std::cout << "[ " << n_ptr->id << "," << n_ptr->resource_pntg << "," <<n_ptr->dedup_num<< "]" <<std::endl;
+				}
+				std::cout << "--------" << std::endl;
+				if(!ts_target_nodes.empty())
+					for(auto n_ptr : ts_target_nodes){
+						std::cout << "[ " << n_ptr->id << "," << n_ptr->resource_pntg << "," <<n_ptr->dedup_num<< "]" <<std::endl;
+					}
+
+#endif 
+				readjust(task,target_nodes,decision);
+				std::cout << "AFTER SCHEDULED readjust: " << std::endl;
+				printResults(decision);
+				if(!mergeResidue(task,decision)) return EXIT_SUCCESS;
+				std::cout << "AFTER SCHEDULED mergeResidue: " << std::endl;
+				printResults(decision);
+
+				failed=true;
+				if(!_useSelfTuning){
+					if(!allocateTimeShare(task,decision, ts_target_nodes)){
+						failed=false;
+					} 
+				}
+				if(failed) return EXIT_FAILURE;
+
+			} 
+			return EXIT_SUCCESS;
+		}
 
 
 
