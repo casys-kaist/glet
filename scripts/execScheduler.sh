5ce6b4b376d5986a4b12f414f9cc2b7ab4eff3c8
csb1024 <csb1024@kaist.ac.kr>
2022-12-12 16:25:30 +0900

    initial commit: example script for executing offline scheduler binary

---
+++ scripts/execScheduler.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+RES_DIR=$PWD/../resource/
+SIM_DIR=$RES_DIR
+BIN=$PWD/../bin/standalone_scheduler
+configfile=sim-config.json
+taskfile=rates.csv
+
+$BIN --resource_dir $RES_DIR --task_config $RES_DIR/$taskfile \
+        --sim_config $SIM_DIR/$configfile --output $RES_DIR/ModelList.txt  \
+        --mem_config $RES_DIR/mem-config.json \
+	--device_config $RES_DIR/device-config.json
+
