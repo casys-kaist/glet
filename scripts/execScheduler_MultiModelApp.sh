769f6b22d28f561cf04269149a3c47cb7bb18cc4
csb1024 <csb1024@kaist.ac.kr>
2023-06-19 17:15:10 +0900

    Initial commit: script for executing scheduler with multi-model application task files

---
+++ scripts/execScheduler_MultiModelApp.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+
+CMP_FLAG=1
+UPDATE_FLAG=1 # flag indicating to update UPDATE_DI
+EXP_FLAG=1
+
+RES_DIR=../resource/
+SIM_DIR=$RES_DIR/SchedConfigs
+BIN=../bin/standalone_scheduler
+CONFIG_DIR=$RES_DIR/MultiModelAppConfigs/
+ROOT_RESULT_DIR=$RES_DIR/MultiModelAppSimulationResults
+
+
+UPDATE_DIR=$RES_DIR/example_output_dir2
+
+
+# Configurations to experiment
+SCENS=('gpulet-int' 'gpulet-noint' 'greedy_part' 'timesharing_only')  
+
+game_intervals=('200' '400' '600' '800' '1000' '1200' '1400' '1100' '1300' '1500' '1600' '1700' '1800')
+game_intervals+=( '1900' '2000' '2100' '2200' '2300' '2400')
+traffic_intervals=('100' '200' '300' '400' '450' '500' '550' '600' '700' '800')
+
+
+WORKLOADS=('game' 'traffic')
+
+
+mkdir -p $ROOT_RESULT_DIR
+
+if [ $EXP_FLAG -eq 1 ];
+then
+
+
+for partition in ${SCENS[@]}
+do
+        RESULT_DIR=$ROOT_RESULT_DIR/$partition
+        mkdir -p $RESULT_DIR
+        rm $RESULT_DIR/*
+
+	for workload in ${WORKLOADS[@]}
+        do
+            if [ "$workload" == 'game' ];then
+                    INTERVALS=${game_intervals[@]}
+            elif [ "$workload" == 'traffic' ]; then
+                    INTERVALS=${traffic_intervals[@]}
+            else
+                    echo "no such workload as $workload"
+                    exit 1
+             fi   
+	    echo $workload
+            echo $partition
+
+            for interval in ${INTERVALS[@]}
+            do
+               echo $interval
+
+		taskfile=tasks_"$workload"_"$interval".csv
+                configfile=config-$partition.json
+                logfile="$workload"-"$interval"-"$partition"-log
+
+		$BIN --resource_dir $RES_DIR --task_config $CONFIG_DIR/$taskfile \
+		--sched_config $SIM_DIR/$configfile --output $RESULT_DIR/ModelList-"$workload"-"$interval".txt \
+		--mem_config $RES_DIR/mem-config.json \
+		--device_config $RES_DIR/device-config.json > "$RESULT_DIR"/$logfile
+
+        	#$BIN --task_config $RES_DIR/multi_configs/$taskfile \
+		#	--sim_config $SIM_DIR/$configfile --mem_config $RES_DIR/mem-config.json \
+		#	--latency_param $RES_DIR/latency.csv  --dev_mem 11019 \
+		#	--output $RESULT_DIR/ModelList-"$workload"-"$interval".txt > "$RESULT_DIR"/$logfile 
+                done
+        done
+done
+fi
+
+#SCENS=('nonpart_inc-5p' 'part_inc-5p' 'part_int_inc-5p' 'nexus_org_inc-5p') 
+
+if [ $UPDATE_FLAG -eq 1 ];then
+    for partition in ${SCENS[@]}
+    do
+        RESULT_DIR=$ROOT_RESULT_DIR/$partition
+        TGT_DIR=$UPDATE_DIR/$partition
+        echo "updating $TGT_DIR "
+        mkdir -p $TGT_DIR
+        cp $RESULT_DIR/* $TGT_DIR/
+    done
+      
+fi
+
+
