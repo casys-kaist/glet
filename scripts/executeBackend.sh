9c1ad6ab757c6072a59ce368ba5a51a97c229ff8
csb1024 <csb1024@kaist.ac.kr>
2023-07-07 14:07:50 +0900

    bug fix: passes the full directory of CWD (instead of the hard coded one)

--- scripts/executeBackend.sh
+++ scripts/executeBackend.sh
@@ -21,6 +21,7 @@ BIN=../bin/backend
 
 proxy_config=$RES_DIR/proxy_config.json
 exp_config=$RES_DIR/config.json
+proxy_dir=$PWD/../resource/proxy/
 
 echo $proxy_config
 
@@ -30,6 +31,7 @@ last_idx=$(($NGPUS-1))
 $BIN --backend_control_portno 8081 --backend_data_portno 8082 \
         --gpu_idxs $first_idx-$last_idx --nproxy $MAX_NUM_OF_PROXYS\
         --config_json $exp_config --proxy_json $proxy_config \
+	--full_proxy_dir $proxy_dir
 	--resource_dir $RES_DIR
 
 
