e95c8f55606e2f18e15cdce5567a74fd6fa5e25b
csb1024 <csb1024@kaist.ac.kr>
2023-07-07 14:31:00 +0900

    bug fix: CUDA_VISIBLE_DEVICES are designated properly now

--- scripts/start_MPS.sh
+++ scripts/start_MPS.sh
@@ -11,18 +11,29 @@ echo "Starting MPS on $NUM GPUs"
 NUM=$((NUM-1))
 GPUS=`seq 0 $NUM`
 
-export CUDA_VISIBLE_DEVICES=$GPUS
+# form comma seperated strings
+
+for id in ${GPUS[@]}
+do
+	str="$str"$id
+	if [ $id -ne $NUM ]
+	then
+		str="$str"","
+	fi
+
+done
+
+export CUDA_VISIBLE_DEVICES=$str
 
 for id in ${GPUS[@]}
 do
     sudo nvidia-smi -i $id -c EXCLUSIVE_PROCESS
 done
 
+
 export CUDA_MPS_PIPE_DIRECTORY=/tmp/nvidia-mps # Select a location that’s accessible to the given $UID
 
 export CUDA_MPS_LOG_DIRECTORY=/tmp/nvidia-log # Select a location that’s accessible to the given $UID
-
-#/usr/local//cuda-10.2/bin/nv-nsight-cu-cli --mode launch nvidia-cuda-mps-control -d # Start the daemon
 
 nvidia-cuda-mps-control -d # Start the daemon
 
